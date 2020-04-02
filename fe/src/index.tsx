462ef54c34cef6b8c571a724a7aa4e91300090e8
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2020-04-02 23:09:44 +0700

    Include global styles.

--- fe/src/index.tsx
+++ fe/src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Home } from './components/home/home';
+import './styles/_main.scss';
 
 ReactDOM.render(
   <Home />,
