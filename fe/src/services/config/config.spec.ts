ab891877ca21d9970a4d1bff717f1934b0de0ba5
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2020-05-02 16:14:12 +0700

    Improve language management and UI.

--- fe/src/services/config/config.spec.ts
+++ fe/src/services/config/config.spec.ts
@@ -7,20 +7,4 @@ describe('config service', () => {
     expect(application.Port).not.toBeNaN();
     expect(application.Server).not.toBeNull();
   });
-
-  it('should contain copyright data', () => {
-    const copyright = Config.getCopyright();
-    expect(copyright.Year).not.toBeNull();
-    expect(copyright.Year).not.toBeNaN();
-    expect(copyright.Author).not.toBeNull();
-  });
-
-  it('should contain google data', () => {
-    const googleParams = Config.getGoogleParams();
-    expect(googleParams.accessType).not.toBeNull();
-    expect(googleParams.client_id).not.toBeNull();
-    expect(googleParams.cookiePolicy).not.toBeNull();
-    expect(googleParams.fetchBasicProfile).toBeTruthy();
-    expect(googleParams.scope).not.toBeNull();
-  });
 });
--- fe/src/services/config/config.ts
+++ fe/src/services/config/config.ts
@@ -4,16 +4,3 @@ export const getApplicationConfig = () => ({
   Port: config.api.port,
   Server: config.api.server,
 });
-
-export const getCopyright = () => ({
-  Year: config.copyright.year,
-  Author: config.copyright.author,
-});
-
-export const getGoogleParams = () => ({
-  client_id: config.google.clientId,
-  scope: config.google.scope,
-  accessType: config.google.accessType,
-  cookiePolicy: config.google.cookiePolicy,
-  fetchBasicProfile: config.google.fetchBasicProfile,
-});
